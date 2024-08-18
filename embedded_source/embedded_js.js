0428a4afc9a5d99bedf3e2001b5cc8e058300827
Asayami <asayamikurashin@gmail.com>
2024-08-18 13:06:01 +0700

    Update UI

--- embedded_source/embedded_js.js
+++ embedded_source/embedded_js.js
@@ -11,7 +11,7 @@ function NavScroll() {
 }
 
 function loadNavbar() {
-  fetch("/clone/theme/navbar.html")
+  fetch("https://asayami.github.io/theme/navbar.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("themeSpace").innerHTML = data;
